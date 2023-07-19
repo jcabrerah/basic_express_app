5a7d2a1ca18013805b5a74f309a6396955eafa45
juan cabrera <juangab@outlook.com>
2023-07-19 13:15:00 -0500

    added prometheus library an default metrics

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const http = require('http')
+const client = require(promt-client)
 const app = express();
+
+const register = new client.Registry();
+
+register.setDafaultLabels({
+    app: 'basic_express_app'
+})
+
+client.collectDefaultMetrics({ register })
 
 app.get('/', (req, res) => {
     res.send('Hello world good word');    
 });
+
+app.get('/metrics', (req, res) => {
+    res.setHeader('Content-type', register.contentType);
+    res.send(register.metrics())
+})
+
 app.listen(8080, () => {
     console.log('Express web app on localhost');
 })
+
